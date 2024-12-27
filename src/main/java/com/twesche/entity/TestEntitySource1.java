5bd7167dd1f28ff4d6de2670ef7a309ed36b8f7f
TrWesche <TrWesche@live.com>
2024-12-27 11:05:58 -0600

    Iterating on Library Foundations

--- src/main/java/com/twesche/entity/TestEntitySource1.java
+++ src/main/java/com/twesche/entity/TestEntitySource1.java
@@ -1,31 +1,30 @@
 package com.twesche.entity;
 
 
-import com.twesche.annotation.EGRColumn;
-import com.twesche.annotation.EGREntity;
-import com.twesche.annotation.EGRMethod;
+import com.twesche.annotation.EGRField;
+import com.twesche.annotation.EGRType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-@EGREntity
+@EGRType
 //@Immutable
 public class TestEntitySource1 {
     @Id
     @GeneratedValue
-    @EGRColumn
+    @EGRField
     public Long id;
 
-    @EGRColumn(canWriteValue = true)
+    @EGRField
     public String value1;
 
-    @EGRColumn(canWriteValue = true)
+    @EGRField
     public String value2;
 
     public Integer value3;
 
-    @EGRMethod
+//    @EGRMethod
     public boolean someMethod()
     {
         return true;
